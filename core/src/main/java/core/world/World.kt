218b9d3e7209b7f104aefcc6c5d7303532532766
Max Leibowitz <403089@bsd48.org>
2020-12-26 10:01:21 -0800

    Created tiles utility class

--- core/src/main/java/core/world/World.kt
+++ core/src/main/java/core/world/World.kt
@@ -4,10 +4,20 @@ import com.badlogic.gdx.math.Vector2
 import ktx.box2d.createWorld
 
 /**
- * A class that holds all the obstacles in the world.
+ * A class that holds all the tiles in the world.
  */
 class World(width: Int, height: Int) {
+    /** The box2d physics world. */
     val box2dWorld = createWorld(Vector2(0f, 0f))
-    /** A 2d array of tiles. */
-    val tiles: Array<Array<Tile>> = Array(height) { y -> Array(width) { x -> Tile(x, y) } }
+    /** An array of tiles, see [Tiles]. */
+    val tiles: Tiles = Tiles(width, height)
+
+    /** Utility class, holds tiles.  You can get tiles out with `tiles.get(x, y)` (java) or `tiles[x, y]` (kotlin) */
+    class Tiles(private val width: Int, private val height: Int) {
+        private val tiles = Array(width * height) { index -> Tile(index % width, index / width) }
+
+        operator fun get(x: Int, y: Int): Tile {
+            return tiles[y * width + x]
+        }
+    }
 }
